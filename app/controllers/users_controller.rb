13a57f6b494fe9f18863263232f56aca2fd96e8d
musaomondi <amokem@yahoo.com>
2016-07-27 20:59:36 +0300

    show flash on successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to ToDo List"
       redirect_to @user
     else 
       render 'new'
