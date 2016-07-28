6edeb13d485d87a1c8b600de73018d08aa01b40f
musaomondi <amokem@yahoo.com>
2016-07-28 20:06:34 +0300

    updating current_user for persistent sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,15 @@ module SessionsHelper
     cookies[:remember_token] = {value: user.remember_token, expires: 7.days.from_now.utc}
   end
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   def logged_in?
     !current_user.nil?
