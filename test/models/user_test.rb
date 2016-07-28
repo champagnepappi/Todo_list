a0bde5815bfd70f6aa75cd43b58ed68a78fe5e11
musaomondi <amokem@yahoo.com>
2016-07-28 06:21:30 +0300

    testing name and email presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,10 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  test "name and email should be present" do
+    @user.name = " "
+    @user.email= " "
+    assert_not @user.valid?
+  end
+    
 end
