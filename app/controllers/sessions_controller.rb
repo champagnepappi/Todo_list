2deb941979ffe75b8b567244eab998fee874fc9c
musaomondi <amokem@yahoo.com>
2016-07-28 13:52:21 +0300

    logging in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params([:session][:password]))
+      log_in user
+      redirect_to user
     else
-      flash.now[:danger]="Recheck you name and password and try again"
+      flash.now[:danger]="Recheck you name or password and try again"
       render 'new'
     end
   end
