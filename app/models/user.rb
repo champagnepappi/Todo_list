6081085f12b3fb9312bc92b9a9f41ee16e036d22
musaomondi <amokem@yahoo.com>
2016-07-28 06:29:16 +0300

    validate email format and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: :true
-  validates :email, presence: :true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: :true, format: {with: VALID_EMAIL_REGEX},
+                                     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, length: {minimum: 6}
 end
