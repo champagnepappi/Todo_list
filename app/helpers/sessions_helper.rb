676366c2a8f549f34aae7bf9e2d63f1667f47490
musaomondi <amokem@yahoo.com>
2016-07-28 20:04:54 +0300

    remembering user for 7days

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  def remember(user)
+    user.remember
+    cookies.signed[:user_id] = {value: user.id, expires: 7.days.from_now.utc}
+    cookies[:remember_token] = {value: user.remember_token, expires: 7.days.from_now.utc}
+  end
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
