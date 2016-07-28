9c933efb651f800e1feca2e7fbe23946b6c555ff
musaomondi <amokem@yahoo.com>
2016-07-28 07:27:43 +0300

    adding a resourse to get the standard RESTful actions for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
 resources :users
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
