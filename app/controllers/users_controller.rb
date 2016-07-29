31f468c12df00ea6aa4ce7dc87552c8609afe4fd
musaomondi <amokem@yahoo.com>
2016-07-29 08:24:59 +0300

    defining the update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
