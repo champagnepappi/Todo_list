dc9825c1616ee0fb393fef9bb2f69ec96daffaa7
musaomondi <amokem@yahoo.com>
2016-07-29 12:24:40 +0300

    activating seed users by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@
 User.create!(name: "Just Example",
              email: "just@example.com",
              password:"1234567",
-             password_confirmation: "1234567")
+             password_confirmation: "1234567",
+             activated: true,
+             activated_at: Time.zone.now)
 9.times do |n|
   name = Faker::Name.name
   email = "just-#{n+1}@example.com"
@@ -16,5 +18,7 @@ User.create!(name: "Just Example",
   User.create!(name: name,
                email: email,
                password:password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
