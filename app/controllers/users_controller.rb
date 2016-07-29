3fc1ff608ba963cc72205c902782d42f8098cd1d
musaomondi <amokem@yahoo.com>
2016-07-29 08:49:44 +0300

    adding a logged_in_user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user=User.new
   end
@@ -30,5 +32,11 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_path
+    end
   end
 end
