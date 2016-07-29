65fa8d40a010f1547537c8b1c866eabe87c84e47
musaomondi <amokem@yahoo.com>
2016-07-29 17:04:21 +0300

    adding a resource for password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
@@ -12,5 +8,5 @@ get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
 resources :account_activations, only: [:edit]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :password_resets, only: [:new, :create, :edit, :update]
 end
