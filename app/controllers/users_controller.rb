d3f725e3f988bda451692881b8fea77687eb88a8
musaomondi <amokem@yahoo.com>
2016-07-29 08:12:04 +0300

    defining the edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
   def show
     @user=User.find_by(id: params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
