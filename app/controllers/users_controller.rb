9a466f0f1d460b3858b6803d83092af0b9ee7eb0
musaomondi <amokem@yahoo.com>
2016-07-29 11:09:47 +0300

    adding correct_user before filter to protect the edit/update pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user=User.new
@@ -38,5 +39,9 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in."
       redirect_to login_path
     end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless @user == current_user
   end
 end
