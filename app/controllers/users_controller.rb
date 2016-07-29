ff67b06e673745312e39533e2bbc5a8b5a5135b0
musaomondi <amokem@yahoo.com>
2016-07-29 11:21:51 +0300

    adding store_location to the logged-in user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,7 @@ class UsersController < ApplicationController
   end
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_path
     end
