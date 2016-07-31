5ffddd0d09a056617e9d39f0e90a907e1a9ad916
musaomondi <amokem@yahoo.com>
2016-07-31 21:30:15 +0300

    generating list model

--- db/migrate/20160730152710_add_user_references_to_list.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReferencesToList < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :lists, :user, foreign_key: true
-  end
-end
--- db/migrate/20160731092341_create_lists.rb
+++ db/migrate/20160731092341_create_lists.rb
@@ -3,8 +3,11 @@ class CreateLists < ActiveRecord::Migration[5.0]
     create_table :lists do |t|
       t.string :title
       t.text :description
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
+    add_foreign_key :lists, :users
+    add_index :lists, [:user_id, :created_at]
   end
 end
