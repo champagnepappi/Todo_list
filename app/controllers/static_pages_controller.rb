084be1f4d38304f6c20e7046c80ee2cc9caf5af5
musaomondi <amokem@yahoo.com>
2016-07-31 21:25:48 +0300

    adding an instance of lists to the users show method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @list = current_user.lists.build 
+      @todo_list =current_user.todo_list
+    end
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   end
   def show
     @user=User.find_by(id: params[:id])
+    @lists= @user.lists
   end
   def edit
     @user = User.find(params[:id])
@@ -36,13 +37,6 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_path
-    end
   end
   def correct_user
     @user = User.find(params[:id])
