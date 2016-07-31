47c2442e360e149113c61e2ba6553260f37d14c1
musaomondi <amokem@yahoo.com>
2016-07-31 21:22:12 +0300

    creating and destroying todo_lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,26 +1,25 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :edit, :update,:create, :destroy]
+  before_action :correct_user, only: :destroy
 
-  def index
-    @lists = List.all
-  end
   def show
-  end
-  def new
-    @list = List.new
+    @list = List.find(params[:id])
   end
   def edit
+    @list = List.find(params[:id])
   end
   def create
-    @list = List.new(list_params)
+    @list = current_user.lists.build(list_params)
     if @list.save
-      redirect_to @list
+      redirect_to root_path
       flash[:notice] = 'List was successfully created.'
     else
-      render :new
+      @todo_list =[]
+      render 'static_pages/home'
     end
   end
   def update
+    @list = List.find(params[:id])
     if @list.update(list_params)
       redirect_to @list
       flash[:notice] = 'List was successfully updated.'
@@ -29,18 +28,13 @@ class ListsController < ApplicationController
     end
   end
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_url
+    redirect_to request.referrer || root_url
     flash[:notice] = 'List was successfully destroyed.'
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_list
-    @list = List.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
   def list_params
     params.require(:list).permit(:title, :description)
   end
