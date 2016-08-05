63a9eb66302242ae824127d2632ddc92b44c80dc
musaomondi <amokem@yahoo.com>
2016-08-05 12:29:12 +0300

    adding test for list show, edit, update actions

--- test/integration/lists_test.rb
+++ test/integration/lists_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ListsTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:musa)
-
+    @list=lists(:one)
   end
   test "list interface" do
     log_in_as(@user)
@@ -28,5 +28,20 @@ class ListsTest < ActionDispatch::IntegrationTest
     #Visit a different user.
     get user_path(users(:benard))
     assert_select 'a', text: 'delete', count: 0
+  end
+  test "should show list" do
+    log_in_as(@user)
+    get list_url(@list)
+    assert_response :success
+  end
+  test "should edit list" do
+    log_in_as(@user)
+    get edit_list_path(@list)
+    assert_response :success
+  end
+  test "should update list" do
+    log_in_as(@user)
+    patch list_url(@list), params: { list: { description: @list.description, title: @list.title } }
+    assert_redirected_to list_path(@list)
   end
 end
