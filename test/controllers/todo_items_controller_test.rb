910449475fff3e7bea07046cd95bd50c617fda86
musaomondi <amokem@yahoo.com>
2016-08-05 12:28:13 +0300

    adding test to create todo items both invalid and valid

--- test/controllers/todo_items_controller_test.rb
+++ test/controllers/todo_items_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TodoItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:musa)
+    @list = lists(:one)
+    @todo_item = todo_items(:one)
+  end
+
+  test "should create todo_item" do
+    log_in_as(@user)
+    get root_path
+    #invalid submission
+    assert_no_difference 'TodoItem.count' do
+      post list_todo_items_path(@list), params: {todo_item: {content: "" }}
+    end
+    #valid submission
+    assert_no_difference 'TodoItem.count', 1 do
+      post list_todo_items_path(@list), params: {todo_item: {content: "This is my todo" }}
+    end
+
+  end
 end
