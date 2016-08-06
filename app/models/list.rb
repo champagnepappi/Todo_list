c611f32d56af41ce57d9fa3b114352a4927833cf
musaomondi <amokem@yahoo.com>
2016-08-06 19:53:54 +0300

    adding a relationship between cards, user and list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
+  enum status: {Public: 0, Private: 1}
   belongs_to :user
-  has_many :todo_items
+  has_many :cards
+  has_many :todo_items, through: :cards
   validates :user_id, presence: true
   validates :title, presence: true
   validates :description, presence:true
