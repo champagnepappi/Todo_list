9075c976ee3f1462567490de89bcb8226815ca4d
musaomondi <amokem@yahoo.com>
2016-08-14 20:18:41 +0300

    adding a relationship between user and lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
-  has_many :todo_items
+  has_many :cards
   validates :user_id, presence: true
   validates :title, presence: true
-  validates :description, presence:true
 end
