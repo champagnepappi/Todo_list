0aa53a73eef9f8e7d77eddcbe639d44b3a41ee10
musaomondi <amokem@yahoo.com>
2016-08-14 20:15:24 +0300

    modifying the todo_items contriller to match the routes

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,32 +1,44 @@
 class TodoItemsController < ApplicationController
-  before_action :set_list
+  before_action :set_card
   def create
-    @todo_item = @list.todo_items.create(todo_item_params)
-    redirect_to @list
+    @todo_item = TodoItem.new(todo_item_params)
+    @card = Card.find(@todo_item.card_id)
+    if @todo_item.save
+      respond_to do |format|
+        format.js {}
+      end
+    end
   end
   def destroy
-    @todo_item = @list.todo_items.find(params[:id])
+    @todo_item = TodoItem.find(params[:id])
     @todo_item.destroy
-    redirect_to @list
+    render nothing: true
+      respond_to do |format|
+        format.js {}
+      end
   end
   def complete
-    @todo_item = @list.todo_items.find(params[:id])
-    @todo_item.update_attribute(:completed_at, Time.now)
-    redirect_to @list
-    flash[:notice] = "Item completed"
+    @todo_item = TodoItem.find(params[:id])
+    if @todo_item.update_attribute(:completed_at, Time.now)
+      respond_to do |format|
+        format.js {}
+      end
+    end
   end
   def unmark
-    @todo_item = @list.todo_items.find(params[:id])
-    @todo_item.update_attribute(:completed_at, nil)
-    redirect_to @list
-    flash[:notice] = "Item unmark as completed"
+    @todo_item = TodoItem.find(params[:id]) 
+    if @todo_item.update_attribute(:completed_at, nil) 
+      respond_to do |format|
+        format.js {}
+      end
+    end
   end
 
   private
-  def set_list
-    @list = List.find(params[:list_id])
+  def set_card
+    @card = Card.find(params[:card_id])
   end
   def todo_item_params
-    params[:todo_item].permit(:content, :user_id, :list_id)
+    params[:todo_item].permit(:content, :card_id)
   end
 end
